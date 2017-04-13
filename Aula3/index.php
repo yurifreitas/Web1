6c0e27f9f15b275695450640ea87816d093567d9
yurifreitas <yuribzfreitas@gmail.com>
2017-04-12 21:19:58 -0300

    Add files via upload

---
+++ Aula3/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<link rel="stylesheet" type="text/css" href="site.css">
+</head>
+<body>
+    
+	<div class="tudo">
+		<header>
+		header	
+		</header>
+<div class="meio">
+    <nav>
+    menu
+    </nav>
+<section>
+    <div class="container">
+    <div class="login">
+        <form method="POST" action="http://127.0.0.1/Aula3/inserir.php">
+
+                    Nome: <input type=text name="nome"><br/>
+                    Idade:<input type=text name="idade"><br/>
+                    Sexo: <br>Feminino  <input type="radio" name="sexo" value="F" checked >
+                              Masculino <input type="radio" name="sexo" value="M"><br/>
+
+                    <input type="submit" name="submit" value="Insert">   
+        </form>
+      </div>
+
+        </div>
+
+ </section>
+</div>
+		
+            
+<footer>Rodape</footer>
+	</div>
+</body>
+</html>
